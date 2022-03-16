85e0bea3803620a1183a74c022713656c0d5f635
daehoKim <kristoff_dh@naver.com>
2022-03-16 16:50:45 +0900

    modify ps1 file and add settingfile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 
+
 oh-my-posh --init --shell pwsh --config ~/.mytheme.omp.json | Invoke-Expression
 
 del alias:curl
@@ -22,6 +23,10 @@ function lt {
     lsd --tree
 }
 
+function ssh-server {
+    ssh dhkristoff@192.168.1.207
+}
+
 function omp-theme {
 
     Get-ChildItem -Path "~\Desktop\window_setting\oh-my-posh theme\*"  -Include '*.omp.json' `
@@ -29,10 +34,28 @@ function omp-theme {
     | ForEach-Object -Process `
     { 
         $esc = [char]27
-        Write-Host "" 
+        Write-Host ">>>" 
         Write-Host "$esc[1m$($_.BaseName)$esc[0m"
         Write-Host ""
         oh-my-posh --config $($_.FullName) --pwd $PWD
         Write-Host ""
+        Write-Host "========================================================"
     }
+}
+
+function space-theme {
+    oh-my-posh --init --shell pwsh `
+        --config '~\Desktop\window_setting\oh-my-posh theme\craver.omp.json' `
+    | Invoke-Expression	
+}
+
+function ssh-con($id, $ip) {
+    [string]$conn_ip = "192.168.1." + $ip
+
+    Write-Output "id : $id"
+    Write-Output "ip : $conn_ip"
+
+    [string]$ssh_ip = $id + '@' + $conn_ip
+
+    ssh $ssh_ip -o ServerAliveInterval=50
 }
