0ebbf155391829134f7b62a0def26c8d33908e16
daehoKim <kristoff_dh@naver.com>
2023-01-04 16:24:40 +0900

    update module and add config file

--- powershell/ssh-util.psm1
+++ powershell/ssh-util.psm1
@@ -5,9 +5,9 @@ Import-Module powershell-yaml
 
 $config_path = "C:/Users/krist/.ssh/remote_ssh_list.yaml"
 
-[System.Collections.ArrayList]$host_list = @()
-function Set-SshList {
-    Write-Host "ssh config path : " $config_path
+function Connect-Ssh {
+    [System.Collections.ArrayList]$host_list = @()
+    Write-Host "ssh config path :" $config_path
 
     $file = Get-Content -Path $config_path -Raw
     $config = ConvertFrom-Yaml ($file)    
@@ -20,21 +20,18 @@ function Set-SshList {
         $SSH_PORT = $ROOT[$idx]['port']
         if ($SSH_PORT) {
             $SSH_ADDR += " -p $SSH_PORT" 
-            $host_list.Add($SSH_ADDR)
+            [void]$host_list.Add($SSH_ADDR)
         }
         else {
-            $host_list.Add($SSH_ADDR)        
+            [void]$host_list.Add($SSH_ADDR)        
         }
-
     }
-}
 
-function Connect-Ssh {
     Write-Host "[Remote SSh List]"
 
     for ($idx = 0; $idx -lt $host_list.Count; ++$idx) {
         Write-Host "---------------------------------"
-        Write-Host "[${idx}] -> " $host_list[$idx]
+        Write-Host "[${idx}] ->" $host_list[$idx]
     }
     Write-Host "---------------------------------"
         
@@ -45,7 +42,4 @@ function Connect-Ssh {
     
 }
 
-Set-SshList
-
-Export-ModuleMember -Function Set-SshList
 Export-ModuleMember -Function Connect-Ssh
